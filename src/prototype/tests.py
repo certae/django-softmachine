8f13bab4e491a666b18678afae74ee9c5bf1199a
Antoine Dubé <antoine.dube@gmail.com>
2013-05-27 09:55:43 -0400

    Ajout de tests pour la classe Diagram, dans le modele prototype.

--- src/prototype/tests.py
+++ src/prototype/tests.py
@@ -630,6 +630,26 @@ class Diagram_ModelTest(TestCase):
         diagram_in_database = Diagram.objects.all()
         self.assertEqual(len(diagram_in_database), 1)
 
+    def test_verifying_diagram_attribute_model_in_database(self):
+        diagram_in_database = Diagram.objects.all()
+        only_entry_in_database = diagram_in_database[0]
+        self.assertEqual(only_entry_in_database.model, self.diagram.model)
+
+    def test_verifying_diagram_attribute_code_in_database(self):
+        diagram_in_database = Diagram.objects.all()
+        only_entry_in_database = diagram_in_database[0]
+        self.assertEqual(only_entry_in_database.code, self.diagram.code)
+
+    def test_verifying_diagram_attribute_description_in_database(self):
+        diagram_in_database = Diagram.objects.all()
+        only_entry_in_database = diagram_in_database[0]
+        self.assertEqual(only_entry_in_database.description, self.diagram.description)
+
+    def test_verifying_diagram_attribute_notes_in_database(self):
+        diagram_in_database = Diagram.objects.all()
+        only_entry_in_database = diagram_in_database[0]
+        self.assertEqual(only_entry_in_database.notes, self.diagram.notes)
+
 
 class DiagramEntity_ModelTest(TestCase):
 
