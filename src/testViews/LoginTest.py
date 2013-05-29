19a359d8968faa88cbcdbb282ac0ac7db0638f9a
Antoine Dubé <antoine.dube@gmail.com>
2013-05-29 11:33:36 -0400

    Ajout de pyvirtualdisplay pour ne pas dependre de l'environnement graphique.

--- src/testViews/LoginTest.py
+++ src/testViews/LoginTest.py
@@ -1,15 +1,19 @@
 from django.test import LiveServerTestCase
+from pyvirtualdisplay import Display
 from selenium import webdriver
 
 
 class LoginTest(LiveServerTestCase):
 
     def setUp(self):
+        self.display = Display(visible=0, size=(1024, 768))
+        self.display.start()
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+        self.display.stop()
 
     def test_can_display_page_with_empty_body_section(self):
         self.browser.get('http://localhost:8000/protoExt/')
