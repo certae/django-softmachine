11da39a5b04c6a77252b527e0b0f34c23ef9d76d
Antoine Dubé <antoine.dube@gmail.com>
2013-05-29 08:48:48 -0400

    Ajout d'un filtre dans la section LOGGING dans settings.py et organisation des tests avec une suite de tests.

--- src/prototype/tests.py
+++ src/prototype/tests.py
@@ -1,33 +1,41 @@
-#from testprototype.ProjectTest import ProjectTest
-#from testprototype.ModelTest import ModelTest
-#from testprototype.EntityTest import EntityTest
-#from testprototype.PropertyBaseTest import PropertyBaseChildTest
-#from testprototype.PropertyTest import PropertyTest
-
-#from testprototype.RelationshipTest import RelationshipTest
-
-#from testprototype.PropertyModelTest import PropertyModelTest
-#from testprototype.PropertyEquivalenceTest import PropertyEquivalenceTest
-#from testprototype.PrototypeTest import PrototypeTest
-#from testprototype.ProtoTableTest import ProtoTableTest
-#from testprototype.DiagramTest import DiagramTest
-#from testprototype.DiagramEntityTest import DiagramEntityTest
-#from testprototype.ServiceTest import ServiceTest
-#from testprototype.ServiceRefTest import ServiceRefTest
-
-# Probleme car Project herite de protoModel et non de models.Model
-
-# Le probleme vient de issubclass()
-
-
 from django.utils.unittest.suite import TestSuite
 from django.utils.unittest.loader import makeSuite
 from testprototype.ProjectTest import ProjectTest
+from testprototype.ModelTest import ModelTest
+from testprototype.EntityTest import EntityTest
+from testprototype.PropertyBaseTest import PropertyBaseChildTest
+from testprototype.PropertyTest import PropertyTest
+from testprototype.RelationshipTest import RelationshipTest
+from testprototype.PropertyModelTest import PropertyModelTest
+from testprototype.PropertyEquivalenceTest import PropertyEquivalenceTest
+from testprototype.PrototypeTest import PrototypeTest
+from testprototype.ProtoTableTest import ProtoTableTest
+from testprototype.DiagramTest import DiagramTest
+from testprototype.DiagramEntityTest import DiagramEntityTest
+from testprototype.ServiceTest import ServiceTest
+from testprototype.ServiceRefTest import ServiceRefTest
 
 
 def suite():
     suite = TestSuite()
 
     suite.addTest(makeSuite(ProjectTest, 'test'))
+    suite.addTest(makeSuite(ModelTest, 'test'))
+    suite.addTest(makeSuite(EntityTest, 'test'))
+
+    suite.addTest(makeSuite(PropertyBaseChildTest, 'test'))
+    suite.addTest(makeSuite(ProjectTest, 'test'))
+    suite.addTest(makeSuite(RelationshipTest, 'test'))
+
+    suite.addTest(makeSuite(PropertyModelTest, 'test'))
+    suite.addTest(makeSuite(PropertyEquivalenceTest, 'test'))
+    suite.addTest(makeSuite(PrototypeTest, 'test'))
+
+    suite.addTest(makeSuite(ProtoTableTest, 'test'))
+    suite.addTest(makeSuite(DiagramTest, 'test'))
+    suite.addTest(makeSuite(DiagramEntityTest, 'test'))
+
+    suite.addTest(makeSuite(ServiceTest, 'test'))
+    suite.addTest(makeSuite(ServiceRefTest, 'test'))
 
     return suite
