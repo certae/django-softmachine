b2e3d1278d3e6d37d1d0fc898fc50ff5335271f0
Antoine Dubé <antoine.dube@gmail.com>
2013-05-30 15:53:04 -0400

    Correction de la syntaxe pour PEP8 et modifications de settings pour ignorer la verification csrf.

--- src/prototype/actions/__init__.py
+++ src/prototype/actions/__init__.py
@@ -3,126 +3,125 @@
 import traceback
 
 from protoLib.utilsBase import slugify
-from protoLib.downloadFile import getFullPath 
+from protoLib.downloadFile import getFullPath
 
 from viewDefinition import getViewDefinition, getViewCode, getEntities
 
 
-def doModelPrototype( modeladmin, request, queryset, parameters):
-    """ 
+def doModelPrototype(modeladmin, request, queryset, parameters):
+    """
     funcion para crear el prototipo sobre 'protoTable' con la definicion del diccionario
-    a partir de Model  
+    a partir de Model
     """
 
-#   El QSet viene con la lista de Ids  
+#   El QSet viene con la lista de Ids
     if queryset.count() == 0:
-        return  {'success':False, 'message' : 'No record selected' }
-        
+        return {'success': False, 'message': 'No record selected'}
+
 #   Mensaje de retorno
-    returnMsg = '' 
+    returnMsg = ''
 
-#   Recorre los registros selccionados   
+#   Recorre los registros selccionados
     for pModel in queryset:
-        returnTmp = getEntities( pModel.entity_set.all() , request , None  )
-        returnMsg += 'Model : ' + pModel.code + ' Entts: ' + returnTmp + '; '    
+        returnTmp = getEntities(pModel.entity_set.all(), request, None)
+        returnMsg += 'Model : ' + pModel.code + ' Entts: ' + returnTmp + '; '
 
-    return {'success':True, 'message' : returnMsg } 
+    return {'success': True, 'message': returnMsg}
 
 
-def doEntityPrototype( modeladmin, request, queryset, parameters ):
+def doEntityPrototype(modeladmin, request, queryset, parameters):
 
-#   El QSet viene con la lista de Ids  
+#   El QSet viene con la lista de Ids
     if queryset.count() != 1:
-        return  {'success':False, 'message' : 'No record selected' }
+        return {'success': False, 'message': 'No record selected'}
 
-    if len( parameters ) != 1: 
-        return  {'success':False, 'message' : 'ViewName required!!' }
+    if len(parameters) != 1:
+        return {'success': False, 'message': 'ViewName required!!'}
 
-#   Recorre los registros selccionados   
-    returnTmp = 'Entt: ' + getEntities( queryset , request, parameters[0]['value']  )
-    return {'success':True, 'message' :  returnTmp } 
+#   Recorre los registros selccionados
+    returnTmp = 'Entt: ' + getEntities(queryset, request, parameters[0]['value'])
+    return {'success': True, 'message': returnTmp}
 
 
-def doPropertyModelJoin( modeladmin, request, queryset, parameters):
-    """ 
-    funcion para unir dos propertyModel 
+def doPropertyModelJoin(modeladmin, request, queryset, parameters):
+    """
+    funcion para unir dos propertyModel
     """
 
-    from propModelJoin import doPropModelJoin 
+    from propModelJoin import doPropModelJoin
 
-#   El QSet viene con la lista de Ids  
+#   El QSet viene con la lista de Ids
     if queryset.count() < 2:
-        return  {'success':False, 'message' : 'Multiple selection required'}
+        return {'success': False, 'message': 'Multiple selection required'}
 
-    return doPropModelJoin ( queryset )
+    return doPropModelJoin(queryset)
 
 
-# -----------  Models  
+# -----------  Models
 
-def doModelGraph( modeladmin, request, queryset, parameters):
-    """ 
-    funcion para crear el modelo grafico 
-    a partir de Model ( doModel )   
-    el proyecto enviara la el QSet de todos los modelos 
+def doModelGraph(modeladmin, request, queryset, parameters):
+    """
+    funcion para crear el modelo grafico
+    a partir de Model ( doModel )
+    el proyecto enviara la el QSet de todos los modelos
     """
 
     from graphModel import generateDotModels
 
-#   El QSet viene con la lista de Ids  
+#   El QSet viene con la lista de Ids
     if queryset.count() != 1:
-        return  {'success':False, 'message' : 'No record selected' }
+        return {'success': False, 'message': 'No record selected'}
 
-            
-#   Envia el QSet con la lista de modelos, 
-    dotdata = generateDotModels ( queryset )
+#   Envia el QSet con la lista de modelos,
+    dotdata = generateDotModels(queryset)
 
-#   Genera el archvivo dot     
-    fileName = 'gm_' + slugify( queryset[0].code ) + '.dot'
-    fullPath = getFullPath( request, fileName )
+#   Genera el archvivo dot
+    fileName = 'gm_' + slugify(queryset[0].code) + '.dot'
+    fullPath = getFullPath(request, fileName)
 
-    fo = open( fullPath , "wb")
-    fo.write( dotdata.encode('utf-8'))
+    fo = open(fullPath, "wb")
+    fo.write(dotdata.encode('utf-8'))
     fo.close()
 
     try:
         import pygraphviz
-        fileNamePdf = fileName.replace( '.dot', '.pdf') 
-        fullPathPdf = getFullPath( request, fileNamePdf )
+        fileNamePdf = fileName.replace('.dot', '.pdf')
+        fullPathPdf = getFullPath(request, fileNamePdf)
 
-        graph = pygraphviz.AGraph( fullPath )
-        graph.layout( prog= 'dot' )
-        graph.draw( fullPathPdf, format ='pdf')
+        graph = pygraphviz.AGraph(fullPath)
+        graph.layout(prog='dot')
+        graph.draw(fullPathPdf, format='pdf')
 
         fileName = fileNamePdf
     except ImportError:
         pass
 
-    return  {'success':True , 'message' : fileName,  'fileName' : fileName }
+    return {'success': True, 'message': fileName, 'fileName': fileName}
 
 
-# ----------------   Project  
+# ----------------   Project
 
-def doImportSchema( modeladmin, request, queryset, parameters):
-    """ 
-    funcion para Importar la def de una Db ( basado en inspectDb ) 
+def doImportSchema(modeladmin, request, queryset, parameters):
+    """
+    funcion para Importar la def de una Db ( basado en inspectDb )
     """
 
-    from reverseDb import getDbSchemaDef 
+    from reverseDb import getDbSchemaDef
 
-#   El QSet viene con la lista de Ids  
+#   El QSet viene con la lista de Ids
     if queryset.count() != 1:
-        return  {'success':False, 'message' : 'No record selected' }
+        return {'success': False, 'message': 'No record selected'}
 
-    try: 
-        getDbSchemaDef( queryset[0] , request  )
+    try:
+        getDbSchemaDef(queryset[0], request)
 #        from multiprocessing import Process
 #        p = Process (target= getDbSchemaDef ,args=( queryset[0] , request ))
 #        p.start()
-    
-#   Recorre los registros selccionados   
+
+#   Recorre los registros selccionados
     except Exception as e:
         traceback.print_exc()
-        return  {'success':False, 'message' : 'Load error' }
+        return {'success': False, 'message': 'Load error'}
         pass
-        
-    return {'success':True, 'message' :  'runing ...' } 
+
+    return {'success': True, 'message': 'running ...'}
