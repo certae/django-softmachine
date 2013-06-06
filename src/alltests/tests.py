a7293e14972a597f2f4134d3e7d8b968bcd2c5d5
Antoine Dubé <antoine.dube@gmail.com>
2013-06-06 16:04:39 -0400

    Suite des tests sur les metadefinitions. INCOMPLET.

--- src/alltests/tests.py
+++ src/alltests/tests.py
@@ -22,7 +22,7 @@ from testprototype.testactions.ViewDefinitionTest import Property2FieldTest
 from testprototype.testactions.ViewDefinitionTest import GetViewDefinitionTest
 from testprototype.testactions.ViewDefinitionTest import GetFkIdTest
 
-from testMetaDefinitions.ObjectsTest import ProjectStructureTest
+from testMetaDefinitions.ObjectsTest import *
 
 from testViews.LoginTest import LoginTest
 
@@ -30,34 +30,48 @@ from testViews.LoginTest import LoginTest
 def suite():
     suite = TestSuite()
 
-    #suite.addTest(makeSuite(ProjectTest, 'test'))
-    #suite.addTest(makeSuite(ModelTest, 'test'))
-    #suite.addTest(makeSuite(EntityTest, 'test'))
+    suite.addTest(makeSuite(ProjectTest, 'test'))
+    suite.addTest(makeSuite(ModelTest, 'test'))
+    suite.addTest(makeSuite(EntityTest, 'test'))
 
-    #suite.addTest(makeSuite(PropertyBaseChildTest, 'test'))
-    #suite.addTest(makeSuite(PropertyTest, 'test'))
-    #suite.addTest(makeSuite(ProjectTest, 'test'))
-    #suite.addTest(makeSuite(RelationshipTest, 'test'))
+    suite.addTest(makeSuite(PropertyBaseChildTest, 'test'))
+    suite.addTest(makeSuite(PropertyTest, 'test'))
+    suite.addTest(makeSuite(ProjectTest, 'test'))
+    suite.addTest(makeSuite(RelationshipTest, 'test'))
 
-    #suite.addTest(makeSuite(PropertyModelTest, 'test'))
-    #suite.addTest(makeSuite(PropertyEquivalenceTest, 'test'))
-    #suite.addTest(makeSuite(PrototypeTest, 'test'))
+    suite.addTest(makeSuite(PropertyModelTest, 'test'))
+    suite.addTest(makeSuite(PropertyEquivalenceTest, 'test'))
+    suite.addTest(makeSuite(PrototypeTest, 'test'))
 
-    #suite.addTest(makeSuite(ProtoTableTest, 'test'))
-    #suite.addTest(makeSuite(DiagramTest, 'test'))
-    #suite.addTest(makeSuite(DiagramEntityTest, 'test'))
+    suite.addTest(makeSuite(ProtoTableTest, 'test'))
+    suite.addTest(makeSuite(DiagramTest, 'test'))
+    suite.addTest(makeSuite(DiagramEntityTest, 'test'))
 
-    #suite.addTest(makeSuite(ServiceTest, 'test'))
-    #suite.addTest(makeSuite(ServiceRefTest, 'test'))
+    suite.addTest(makeSuite(ServiceTest, 'test'))
+    suite.addTest(makeSuite(ServiceRefTest, 'test'))
 
-    #suite.addTest(makeSuite(LoginTest, 'test'))
+    suite.addTest(makeSuite(LoginTest, 'test'))
 
-    #suite.addTest(makeSuite(BaseDefinitionTest, 'test'))
-    #suite.addTest(makeSuite(GetViewCodeTest, 'test'))
-    #suite.addTest(makeSuite(Property2FieldTest, 'test'))
-    #suite.addTest(makeSuite(GetViewDefinitionTest, 'test'))
-    #suite.addTest(makeSuite(GetFkIdTest, 'test'))
+    suite.addTest(makeSuite(BaseDefinitionTest, 'test'))
+    suite.addTest(makeSuite(GetViewCodeTest, 'test'))
+    suite.addTest(makeSuite(Property2FieldTest, 'test'))
+    suite.addTest(makeSuite(GetViewDefinitionTest, 'test'))
+    suite.addTest(makeSuite(GetFkIdTest, 'test'))
 
     suite.addTest(makeSuite(ProjectStructureTest, 'test'))
+    suite.addTest(makeSuite(ModelStructureTest, 'test'))
+    suite.addTest(makeSuite(EntityStructureTest, 'test'))
+
+    suite.addTest(makeSuite(PropertyStructureTest, 'test'))
+    suite.addTest(makeSuite(ProjectStructureTest, 'test'))
+    #suite.addTest(makeSuite(RelationshipStructureTest, 'test'))
+    suite.addTest(makeSuite(PropertyModelStructureTest, 'test'))
+    suite.addTest(makeSuite(PropertyEquivalenceStructureTest, 'test'))
+    suite.addTest(makeSuite(PrototypeStructureTest, 'test'))
+    suite.addTest(makeSuite(ProtoTableStructureTest, 'test'))
+    #suite.addTest(makeSuite(DiagramStructureTest, 'test'))
+    #suite.addTest(makeSuite(DiagramEntityStructureTest, 'test'))
+    #suite.addTest(makeSuite(ServiceStructureTest, 'test'))
+    #suite.addTest(makeSuite(ServiceRefStructureTest, 'test'))
 
     return suite
--- src/testMetaDefinitions/ObjectsTest.py
+++ src/testMetaDefinitions/ObjectsTest.py
@@ -4,17 +4,158 @@ from pprint import pprint
 from django.test import TestCase
 import django.utils.simplejson as json
 
-from prototype.models import Project
+from prototype.models import *
 
 
-def validatePair(key, value):
-    pass
+def getFields(modelclass):
+    fields = []
+    for value in modelclass.protoExt:
+            #print(value)
+            fields.append(value)
+    return fields
+
+
+def getValue(modelclass, field):
+        if type(modelclass.protoExt[field]) != list:
+            for values in Project.protoExt[field]:
+                return values
+        else:
+            for values in modelclass.protoExt[field][0]:
+                return values
+
+
+def validatePair(field, value, datatree):
+    status = False
+    for items in datatree['pcl']:
+        if field in datatree['pcl'][items]:
+            status = True
+            break
+
+    if 'listOf' in datatree[field]:
+        field = datatree[field]['listOf']
+
+    if status is False:
+        return False
+
+    status = False
+    if 'lists' in datatree[field] and value in datatree[field]['lists']:
+        status = True
+    elif 'properties' in datatree[field] and value in datatree[field]['properties']:
+        status = True
+
+    return status
+
+
+def checkAllFields(modelclass, datatree):
+    fields = getFields(modelclass)
+
+    for index in range(0, len(fields)):
+        value = getValue(modelclass, fields[index])
+        if validatePair(fields[index], value, datatree) is False:
+            return False
+
+    return True
 
 
 class ProjectStructureTest(TestCase):
     def setUp(self):
         self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
-        pprint(self.MetaObjects)
 
-    def test_field_actions(self):
-        pass
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(Project, self.MetaObjects))
+
+
+class ModelStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(Model, self.MetaObjects))
+
+
+class EntityStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(Entity, self.MetaObjects))
+
+
+class PropertyStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(Property, self.MetaObjects))
+
+
+class RelationshipStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(Relationship, self.MetaObjects))
+
+
+class PropertyModelStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(PropertyModel, self.MetaObjects))
+
+
+class PropertyEquivalenceStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(PropertyEquivalence, self.MetaObjects))
+
+
+class PrototypeStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(Prototype, self.MetaObjects))
+
+
+class ProtoTableStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(ProtoTable, self.MetaObjects))
+
+
+class DiagramStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(Diagram, self.MetaObjects))
+
+
+class DiagramEntityStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(DiagramEntity, self.MetaObjects))
+
+
+class ServiceStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(Service, self.MetaObjects))
+
+
+class ServiceRefStructureTest(TestCase):
+    def setUp(self):
+        self.MetaObjects = json.loads(open('src/testMetaDefinitions/MetaObjects.dat').read())
+
+    def test_field_and_value(self):
+        self.assertTrue(checkAllFields(ServiceRef, self.MetaObjects))
