8e761e7800a15d633d833431ed1d04b0feb1a1f5
Antoine Dubé <antoine.dube@gmail.com>
2013-06-25 11:19:00 -0400

    Reusinage des tests dans ViewCreationTest.py

--- src/testSampleProject/ViewCreationTest.py
+++ src/testSampleProject/ViewCreationTest.py
@@ -85,15 +85,15 @@ class GetEntitiesTest(TestCase):
 
     def setUp(self):
         self.entity = Entity.objects.all()
+        self.request = HttpRequest()
+        self.request.user = 26
+        self.viewTitleString = 'testViewTitle'
 
     def tearDown(self):
         pass
 
     def test_getentities(self):
-        request = HttpRequest()
-        request.user = 26
-        viewTitleString = 'testViewTitle'
-        returnMessage = getEntities(self.entity, request, viewTitleString)
+        returnMessage = getEntities(self.entity, self.request, self.viewTitleString)
         self.assertEqual(self.entity[0].code + ',' + self.entity[1].code + ',', returnMessage)
 
 
@@ -101,30 +101,25 @@ class DoModelPrototypeTest(TestCase):
     fixtures = ['auth.json', 'protoLib.json', 'prototype.json']
 
     def setUp(self):
-        pass
+        self.model = Model.objects.all()
+        self.request = HttpRequest()
+        self.request.user = 26
 
     def tearDown(self):
         pass
 
     def test_DoModelPrototype_NonEmptyQuerySet(self):
-        model = Model.objects.all()
-        request = HttpRequest()
-        request.user = 26
-        returnMessage = doModelPrototype('', request, model, '')
+        returnMessage = doModelPrototype('', self.request, self.model, '')
         self.assertEqual(returnMessage['message'], 'Model : t_model Entts: t_entity,t_other_entity,; ')
         self.assertTrue(returnMessage['success'])
 
     def test_DoModelPrototype_EmptyQuerySet(self):
-        request = HttpRequest()
-        request.user = 26
-
-        model = Model.objects.all()
-        for entries in model:
+        for entries in self.model:
             entries.delete()
-        model = Model.objects.all()
-        self.assertTrue(model.count() == 0)
+        self.model = Model.objects.all()
+        self.assertTrue(self.model.count() == 0)
 
-        returnMessage = doModelPrototype('', request, model, '')
+        returnMessage = doModelPrototype('', self.request, self.model, '')
         self.assertEqual(returnMessage['message'], 'No record selected')
         self.assertFalse(returnMessage['success'])
 
@@ -133,32 +128,25 @@ class DoEntityPrototypeTest(TestCase):
     fixtures = ['auth.json', 'protoLib.json', 'prototype.json']
 
     def setUp(self):
-        pass
+        self.request = HttpRequest()
+        self.request.user = 26
+
+        self.model = Model.objects.all()
 
     def tearDown(self):
         pass
 
     def test_DoEntityPrototype_validquery_validparameters(self):
-        request = HttpRequest()
-        request.user = 26
-
-        model = Model.objects.all()
-
         parameters = []
         parameters.append({'value': 'valueOfParameter'})
 
-        for pModel in model:
+        for pModel in self.model:
             pModel.entity_set.all()[1].delete()  # Doit avoir seulement 1 entity
-            returnMessage = doEntityPrototype('', request, pModel.entity_set.all(), parameters)
+            returnMessage = doEntityPrototype('', self.request, pModel.entity_set.all(), parameters)
             self.assertTrue(returnMessage['success'])
 
     def test_DoEntityPrototype_validquery_invalidparameters(self):
-        request = HttpRequest()
-        request.user = 26
-
-        model = Model.objects.all()
-
-        for pModel in model:
+        for pModel in self.model:
             pModel.entity_set.all()[1].delete()  # Doit avoir seulement 1 entity
-            returnMessage = doEntityPrototype('', request, pModel.entity_set.all(), '')
+            returnMessage = doEntityPrototype('', self.request, pModel.entity_set.all(), '')
             self.assertFalse(returnMessage['success'])
