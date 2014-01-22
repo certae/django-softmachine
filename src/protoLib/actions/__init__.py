980bf6454713b3d56eb40b48f970e474d7283278
dario <dariogomezt@gmail.com>
2014-01-22 08:29:06 -0500

    Wflow resume

--- src/protoLib/actions/__init__.py
+++ src/protoLib/actions/__init__.py
@@ -6,14 +6,26 @@ def doWFlowResume(modeladmin, request, queryset, parameters):
     Genera un resumen de las novedades para WFlow, 
     se apoya en la tabla de parametros para obtner las entidades wflow y la llave   
     """
+
+    from protoLib.models import ParametersBase, getDjangoModel, WflowAdminResume
         
 #   Mensaje de retorno
     returnMsg = '' 
 
-    from protoLib.models import ParametersBase 
+#   Borra la tabla de resumen 
+    WflowAdminResume.objects.all().delete()
 
+#   Recorre los parametros para conocer las tablas de wFlow 
     Qs = ParametersBase.objects.filter(parameterKey='wflow')
     for pParam in Qs:
-        pass 
+        modelName = getDjangoModel(pParam.parameterValue)
+        wfStatus  = pParam.parameterValue or '0'
+
+        try:    
+            wfModel =  getDjangoModel( modelName )
+        except :
+            continue 
+        
+        wfQs = wfModel.objects.filter( smWflowStatus = wfStatus ).count()
 
     return {'success':True, 'message' : returnMsg }  
