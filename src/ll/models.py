a088f3242c1671955d5ee8729cb871db3e66ab21
Marieme <douarime@gmail.com>
2014-01-27 15:38:22 -0500

    Update models.py

--- src/ll/models.py
+++ src/ll/models.py
@@ -45,8 +45,9 @@ class Logiciel(ProtoModel):
                             'methode' : '', 
                             'change' : ( 'I', 'Ok' ),
                             'setOwner' : True , 
-                            'notifyOwner' : True , 
-                            'message' : 'Accepté' , 
+                            'notifyOwner' : True ,    
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nNous avons accepté l\'enregistrement <sk> dans <concept>, en date du <date>.\n\n<admin>','message' : 'Accepté' , 
                             'admMessagePropmt' : '',   
                         }, {
                             'name' : 'reject', 
@@ -56,13 +57,11 @@ class Logiciel(ProtoModel):
                             'setOwner' : False , 
                             'notifyOwner' : True , 
                             'emailNotification' : True,
-                            'emailTemplate' : 'M/Mme. %User>  on a refuse l\'enregistrement <concept>,<sk> en date <date>  parce que <admmessage> appelle moi <admin>',  
+                            'emailTemplate' : 'Bonjour <User>,\n\nDésolés, nous avons refusé l\'enregistrement <sk> dans <concept>, en date du <date> parce que <admmessage>.\n\n<admin>',  
                             'admMessagePropmt' : 'Raison de refuse?',   
                         }
                     ] 
                   } 
-
-#TODO Courriel 
 
     def __unicode__(self):
         return slugify(self.nom_logiciel)
@@ -93,6 +92,51 @@ class Licence(ProtoModel):
     type_licence = models.CharField(blank= True, null= True, max_length= 255)
     uri_licence = models.CharField(blank= True, null= True, max_length= 255)
 
+    _WorkFlow =  {  'initialStatus' :   'I', 
+                    'OkStatus' : 'Ok', 
+                    'wfFilters' : [
+                        {
+                            'name': 'initial',
+                            'menuText': 'À verifier',
+                            'wfStatus' : 'I'
+                        }, {
+                            'name': 'ok',
+                            'menuText': 'Accepté',
+                            'wfStatus' : 'Ok'
+                        }, {
+                            'name': 'novalides',
+                            'menuText': 'Refusé',
+                            'wfStatus' : 'R'
+                        }
+                    ],
+
+                    'transitions' : [
+                         {
+                            'name' : 'accept', 
+                            'menuText' : 'Accepter', 
+                            'viewIcon' : '', 
+                            'descripion' : '', 
+                            'methode' : '', 
+                            'change' : ( 'I', 'Ok' ),
+                            'setOwner' : True , 
+                            'notifyOwner' : True ,    
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nNous avons accepté l\'enregistrement <sk> dans <concept>, en date du <date>.\n\n<admin>','message' : 'Accepté' , 
+                            'admMessagePropmt' : '',   
+                        }, {
+                            'name' : 'reject', 
+                            'menuText' : 'Refuser', 
+                            'methode' : '', 
+                            'change' : ( 'I', 'R' ),
+                            'setOwner' : False , 
+                            'notifyOwner' : True , 
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nDésolés, nous avons refusé l\'enregistrement <sk> dans <concept>, en date du <date> parce que <admmessage>.\n\n<admin>',  
+                            'admMessagePropmt' : 'Raison de refuse?',   
+                        }
+                    ] 
+                  } 
+                  
     def __unicode__(self):
         return slugify(self.achronyme_licence)
 
@@ -152,6 +196,51 @@ class LangageUtilise(ProtoModel):
 
 class Langage(ProtoModel):
     nom_langage = models.CharField(blank= False, null= False, max_length= 255)
+    
+    _WorkFlow =  {  'initialStatus' :   'I', 
+                    'OkStatus' : 'Ok', 
+                    'wfFilters' : [
+                        {
+                            'name': 'initial',
+                            'menuText': 'À verifier',
+                            'wfStatus' : 'I'
+                        }, {
+                            'name': 'ok',
+                            'menuText': 'Accepté',
+                            'wfStatus' : 'Ok'
+                        }, {
+                            'name': 'novalides',
+                            'menuText': 'Refusé',
+                            'wfStatus' : 'R'
+                        }
+                    ],
+
+                    'transitions' : [
+                         {
+                            'name' : 'accept', 
+                            'menuText' : 'Accepter', 
+                            'viewIcon' : '', 
+                            'descripion' : '', 
+                            'methode' : '', 
+                            'change' : ( 'I', 'Ok' ),
+                            'setOwner' : True , 
+                            'notifyOwner' : True ,    
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nNous avons accepté l\'enregistrement <sk> dans <concept>, en date du <date>.\n\n<admin>','message' : 'Accepté' , 
+                            'admMessagePropmt' : '',   
+                        }, {
+                            'name' : 'reject', 
+                            'menuText' : 'Refuser', 
+                            'methode' : '', 
+                            'change' : ( 'I', 'R' ),
+                            'setOwner' : False , 
+                            'notifyOwner' : True , 
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nDésolés, nous avons refusé l\'enregistrement <sk> dans <concept>, en date du <date> parce que <admmessage>.\n\n<admin>',  
+                            'admMessagePropmt' : 'Raison de refuse?',   
+                        }
+                    ] 
+                  } 
 
     def __unicode__(self):
         return slugify(self.nom_langage)
@@ -161,6 +250,51 @@ class Langage(ProtoModel):
 
 class SystemeExploitation(ProtoModel):
     nom_systeme_exploitation = models.CharField(blank= False, null= False, max_length= 255)
+    
+    _WorkFlow =  {  'initialStatus' :   'I', 
+                    'OkStatus' : 'Ok', 
+                    'wfFilters' : [
+                        {
+                            'name': 'initial',
+                            'menuText': 'À verifier',
+                            'wfStatus' : 'I'
+                        }, {
+                            'name': 'ok',
+                            'menuText': 'Accepté',
+                            'wfStatus' : 'Ok'
+                        }, {
+                            'name': 'novalides',
+                            'menuText': 'Refusé',
+                            'wfStatus' : 'R'
+                        }
+                    ],
+
+                    'transitions' : [
+                         {
+                            'name' : 'accept', 
+                            'menuText' : 'Accepter', 
+                            'viewIcon' : '', 
+                            'descripion' : '', 
+                            'methode' : '', 
+                            'change' : ( 'I', 'Ok' ),
+                            'setOwner' : True , 
+                            'notifyOwner' : True ,    
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nNous avons accepté l\'enregistrement <sk> dans <concept>, en date du <date>.\n\n<admin>','message' : 'Accepté' , 
+                            'admMessagePropmt' : '',   
+                        }, {
+                            'name' : 'reject', 
+                            'menuText' : 'Refuser', 
+                            'methode' : '', 
+                            'change' : ( 'I', 'R' ),
+                            'setOwner' : False , 
+                            'notifyOwner' : True , 
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nDésolés, nous avons refusé l\'enregistrement <sk> dans <concept>, en date du <date> parce que <admmessage>.\n\n<admin>',  
+                            'admMessagePropmt' : 'Raison de refuse?',   
+                        }
+                    ] 
+                  } 
 
     def __unicode__(self):
         return slugify(self.nom_systeme_exploitation)
@@ -577,7 +711,52 @@ class OrganismePublic(ProtoModel):
     nom = models.CharField(blank= True, null= True, max_length= 255)
     mission = models.TextField(blank = True, null = True)
     numero_sct = models.CharField(blank= True, null= True, max_length= 255)
+    
+    _WorkFlow =  {  'initialStatus' :   'I', 
+                    'OkStatus' : 'Ok', 
+                    'wfFilters' : [
+                        {
+                            'name': 'initial',
+                            'menuText': 'À verifier',
+                            'wfStatus' : 'I'
+                        }, {
+                            'name': 'ok',
+                            'menuText': 'Accepté',
+                            'wfStatus' : 'Ok'
+                        }, {
+                            'name': 'novalides',
+                            'menuText': 'Refusé',
+                            'wfStatus' : 'R'
+                        }
+                    ],
 
+                    'transitions' : [
+                         {
+                            'name' : 'accept', 
+                            'menuText' : 'Accepter', 
+                            'viewIcon' : '', 
+                            'descripion' : '', 
+                            'methode' : '', 
+                            'change' : ( 'I', 'Ok' ),
+                            'setOwner' : True , 
+                            'notifyOwner' : True ,    
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nNous avons accepté l\'enregistrement <sk> dans <concept>, en date du <date>.\n\n<admin>','message' : 'Accepté' , 
+                            'admMessagePropmt' : '',   
+                        }, {
+                            'name' : 'reject', 
+                            'menuText' : 'Refuser', 
+                            'methode' : '', 
+                            'change' : ( 'I', 'R' ),
+                            'setOwner' : False , 
+                            'notifyOwner' : True , 
+                            'emailNotification' : True,
+                            'emailTemplate' : 'Bonjour <User>,\n\nDésolés, nous avons refusé l\'enregistrement <sk> dans <concept>, en date du <date> parce que <admmessage>.\n\n<admin>',  
+                            'admMessagePropmt' : 'Raison de refuse?',   
+                        }
+                    ] 
+                  } 
+                  
     def __unicode__(self):
         return slugify(self.acronyme)
 
