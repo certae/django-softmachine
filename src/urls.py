c39601c2f455e48dc7748426b470ce90bec60654
Victorette <giovanni.victorette@gmail.com>
2014-02-03 09:32:02 -0500

    urls config

--- src/urls.py
+++ src/urls.py
@@ -1,28 +1,36 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import direct_to_template
-from settings import PPATH
+from settings import PPATH, DEBUG
 
 # Uncomment the next two lines to enable the admin:
 import django.contrib.admin
 django.contrib.admin.autodiscover()
 
 
-
 urlpatterns = patterns('',
     url(r'^admin/', include(django.contrib.admin.site.urls)) ,
 
-    url(r'^protoExt$', direct_to_template, { 'template': 'protoExt.html'} ),
-    url(r'^protoExtReset$', direct_to_template, { 'template': 'protoExt.html', 'extra_context': { 'isPasswordReseted': True, }} ),
     url(r'^protoLib/', include('protoLib.urls')),
 
 #    url(r'^prueba$', direct_to_template, { 'template': 'prueba.html' }),
 
 #    Use for production instalation and for load json configuration files
-    url(r'static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': PPATH + '/static'}),
-    url(r'resources/(?P<path>.*)$', 'django.views.static.serve',{'document_root': PPATH + '/static'}),
-    url(r'media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': PPATH + '/static'}),
+    url(r'static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PPATH + '/static'}),
+    url(r'resources/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PPATH + '/static'}),
+    url(r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': PPATH + '/static'}),
 
 #   Archivos generados
     url(r'getFile/(?P<path>.*)$', 'protoLib.downloadFile.getFile', {}),
 )
 
+if DEBUG :
+    urlpatterns += patterns('',
+        url(r'^protoExt$', direct_to_template, { 'template': 'protoExt.html'}),
+        url(r'^protoExtReset$', direct_to_template, { 'template': 'protoExt.html', 'extra_context': { 'isPasswordReseted': True, }}),
+    )
+else :
+    urlpatterns += patterns('',
+        url(r'^protoExt$', direct_to_template, { 'template': 'protoExt_production.html'}),
+        url(r'^protoExtReset$', direct_to_template, { 'template': 'protoExt.html', 'extra_context': { 'isPasswordReseted': True, }}),
+    )
+    
