92e4a4051a29c6cbfbb62703230bb671942b788f
Victorette <giovanni.victorette@gmail.com>
2014-06-12 15:26:09 -0400

    App css

--- src/protoLib/static/js/globals/configProperties.js
+++ src/protoLib/static/js/globals/configProperties.js
@@ -5,7 +5,16 @@
 _SM._requiredField = '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>';
 
 _SM._versionProto = 'Version 1.0.2';
-_SM._siteTitle = '<span class="title"> CeRTAE </span><br><span class="subtitle">' + _SM._versionProto + '</span>';
+_SM._siteTitle = '<div class="top-piv">' + 
+    '<div class="piv">' +
+        '<div class="logo">' +
+        '</div>' +
+        '<div class="content">' +
+            '<div class="title"> CeRTAE </div>' +
+            '<div class="subtitle">' + _SM._versionProto + '</div>' +
+        '</div>' +
+    '</div>' +
+'</div>';
 
 _SM.loginTitle = 'Identification';
 _SM._siteTitleCollapsed = false;
