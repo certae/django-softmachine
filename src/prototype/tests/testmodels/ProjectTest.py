01cdef1c1fba0268904d81e974ca418a8856e3e7
Victorette <giovanni.victorette@gmail.com>
2014-06-12 14:21:08 -0400

    Organizing tests

--- src/prototype/tests/testmodels/ProjectTest.py
+++ src/prototype/tests/testmodels/ProjectTest.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from prototype.models import Project
-from prototype.testprototype.testmodels.TestUtilities import createTestProject
+from prototype.tests.testmodels.TestUtilities import createTestProject
 
 
 class ProjectTest(TestCase):
