93183c220870eed4a2e75f968c0ecddb6a9e3aef
dario <dariogomezt@gmail.com>
2014-06-18 09:00:42 -0400

    Merge pipy cmds -> scripts [apps] - proto[apps]

--- src/rai/outils/doMySQLBackup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-now="$(date +'%d-%m-%y')"
-mysqldump -u manager -ppassword prototypeur > "mysql.bak-"$now".sql"
-mysqldump -u manager -ppassword rai > "mysql-rai.bak-"$now".sql"
--- src/rai/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from rai.raccordement import getModeleRaccordement, createRaccordement, deleteRaccordement, listRaccordement
-
-urlpatterns = patterns('',
-    url('getModeleRaccordement/$', getModeleRaccordement),
-    url('createRaccordement/$', createRaccordement),
-    url('deleteRaccordement/$', deleteRaccordement),
-    url('listRaccordement/$', listRaccordement),
-)
