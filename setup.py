4f8ef9fa3fa2e96d09fa4c17236b3648661c8cdf
dariogt <dariogomezt@gmail.com>
2014-11-06 13:47:10 -0500

    Preparing distribution

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-softmachine',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages('src',exclude=['alltests', 'softmachine', 'alltests.*']),
     package_dir = {'':'src'},
     include_package_data=True,
